78da5132370b5f667bd06c40cde9701c004105af
Glenn Haertlein <glenn.hline@outlook.com>
2025-05-15 14:41:34 -0400

    Add post class to css and apply to post component

--- react-crash-course/src/App.jsx
+++ react-crash-course/src/App.jsx
@@ -2,7 +2,13 @@ import Post from "./components/Post";
 
 function App() {
   return (
-    <Post/>
+    <main>
+      <h1>My first React app</h1>
+      <Post name="Glenn" body="ReactJS has a lot of moving parts."/>
+      <Post name="Jim" body="Glenn's too old to learn this stuff."/>
+      <Post name="Anne" body="Agree. He should have invested at a muuch younger age. Then he wouldn't still be working."/>
+      <Post name="anonymous" body="What a LOSER! HAHAHAHA!"/>
+    </main>
   );
 }
 
--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,12 +1,9 @@
-const names = ['Glenn', 'Jane'];
-
-function Post() {
-    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post({name, body}) {
+    
     return(
-        <div>
-            <p>{chosenName}</p>
-            <p>React.js is awesome!</p>
+        <div className='post'>
+            <p>{name}</p>
+            <p>{body}</p>
         </div>
     )
 }
