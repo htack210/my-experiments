45bcb23effbb66ed32c4c3e269ea48fc93bc82e2
Glenn Haertlein <htack210@live.com>
2025-05-16 18:16:59 -0400

    Build an example of lifting state. Set default values for the form.

--- react-crash-course/src/components/PostsList.jsx
+++ react-crash-course/src/components/PostsList.jsx
@@ -1,14 +1,35 @@
 import Post from './Post'
+import NewPost from './NewPost'
 import classes from './PostsList.module.css';
+import { useState } from 'react';
+
+const DEFAULT_BODY = 'Enter text';
+const DEFAULT_AUTHOR = 'Enter author name';
 
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('Enter text')
+    const [enteredAuthor, setEnteredAuthor] = useState('Enter author name')
+
+    function bodyChangeHandler(event) {
+        const value = event.target.value
+        setEnteredBody(value == '' ? DEFAULT_BODY : value);
+    }
+
+    function authorChangeHandler(event) {
+        const value = event.target.value
+        setEnteredAuthor(value == '' ? DEFAULT_AUTHOR : value);
+    }
+
     return (
-        <ul className={classes.posts}>
-            <Post name="Glenn" body="ReactJS has a lot of moving parts." />
-            <Post name="Jim" body="Glenn's too old to learn this stuff." />
-            <Post name="Anne" body="Agree. He should have invested at a much younger age. Then he wouldn't still be working." />
-            <Post name="anonymous" body="What a LOSER! HAHAHAHA!" />
-        </ul>
+        <>
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
+            <ul className={classes.posts}>
+                <Post name={enteredAuthor} body={enteredBody} />
+                <Post name="Jim" body="Glenn's too old to learn this stuff." />
+                <Post name="Anne" body="Agree. He should have invested at a much younger age. Then he wouldn't still be working." />
+                <Post name="anonymous" body="What a LOSER! HAHAHAHA!" />
+            </ul>
+        </>
     )
 }
 
