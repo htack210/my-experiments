1c5a31dcac2e833b9e3822b8823828760604feb6
Glenn Haertlein <glenn.hline@outlook.com>
2025-05-21 10:27:18 -0400

    Add paths

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 import App from './App'
+import NewPost from './components/NewPost'
+import Modal from './components/Modal'  
 import './index.css'
+
+const router = createBrowserRouter([
+    {path: '/', element: <App />},
+    {path: '/create-post', element: <NewPost />},
+    {path: '/post-detail', element: <Modal />},
+  ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
