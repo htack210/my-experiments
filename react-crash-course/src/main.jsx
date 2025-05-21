5115985bf63e7bdbaaab263168431d377db291ff
Glenn Haertlein <htack210@live.com>
2025-05-21 17:12:19 -0400

    Add layout routes

--- react-crash-course/src/main.jsx
+++ react-crash-course/src/main.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {RouterProvider, createBrowserRouter} from 'react-router-dom'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App'
 import NewPost from './components/NewPost'
-import Modal from './components/Modal'  
+import { RootLayout } from './routes/RootLayout'
 import './index.css'
 
 const router = createBrowserRouter([
-    {path: '/', element: <App />},
-    {path: '/create-post', element: <NewPost />},
-    {path: '/post-detail', element: <Modal />},
-  ]);
+  {
+    path: '/',
+    element: <RootLayout />, children: [
+      { path: '/', element: <App /> },
+      { path: '/create-post', element: <NewPost /> },
+    ]
+  }
+
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
